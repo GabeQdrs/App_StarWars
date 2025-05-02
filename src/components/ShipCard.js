337d365052a8360fcd176c78ae3601ec5db0debf
Juan Formaio <juanformaio@gmail.com>
2025-05-02 13:52:52 -0300

    Teste StarShips

---
+++ src/components/ShipCard.js
@@ -0,0 +1,32 @@
+// ShipCard.js
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+export default function ShipCard({ ship }) {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>{ship.title}</Text>
+      <Text>Modelo: {ship.model}</Text>
+      <Text>Passageiros: {ship.passenger}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    margin: 10,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
--- src/screens/StarshipsScreen.js
+++ src/screens/StarshipsScreen.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// StarshipsScreen.js
+import React, { useEffect, useState } from 'react';
+import { FlatList, ActivityIndicator, View, StyleSheet } from 'react-native';
+import axios from 'axios';
+import { useRoute } from '@react-navigation/native';
+import ShipCard from '../components/ShipCard'; 
 
-export default function StarshipsScreen() {
+export default function StarshipsScreen({ route }) {
+  const [ships, setShips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { character } = route.params;
+
+  async function fetchShips() {
+    try {
+      setLoading(true);
+      const responses = await Promise.all(character.ships.map(url => axios.get(url)));
+      setShips(responses.map(res => res.data));
+    } catch (error) {
+      console.error("Erro ao buscar naves:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+  fetchShips();
+  }, []);
+
+  if (loading) return <ActivityIndicator size="large" color="#000" />;
+
+
   return (
-    <View>
-      <Text>StarshipsScreen</Text>
-    </View>
-  )
+    <FlatList
+      data={ships}
+      renderItem={({ item }) => <ShipCard ship={item} />}
+      keyExtractor={(item) => item.episode_id.toString()}
+    />
+  );
 }
+
